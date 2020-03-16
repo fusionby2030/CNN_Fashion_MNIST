895a9387578baf448d39be610df1e78e0aecdcc1
Adam Kit <adamkit11@gmail.com>
2020-03-16 07:21:54 +0100

    added .gitignore

--- fashion_mnist_loaded.py
+++ fashion_mnist_loaded.py
@@ -6,34 +6,31 @@ from keras.utils import np_utils
 from sklearn.metrics import classification_report
 # load model
 model = load_model('model.h5')
-# summarize model.
+#Will print a model Summary
 model.summary()
-# load dataset
+# load dataset like in fashion_mnist.py
 ((trainX, trainY), (testX, testY)) = fashion_mnist.load_data()
-# if we are using "channels first" ordering, then reshape the design
-# matrix such that the matrix is:
-# 	num_samples x depth x rows x columns
 if K.image_data_format() == "channels_first":
 	trainX = trainX.reshape((trainX.shape[0], 1, 28, 28))
 	testX = testX.reshape((testX.shape[0], 1, 28, 28))
-
-# otherwise, we are using "channels last" ordering, so the design
-# matrix shape should be: num_samples x rows x columns x depth
 else:
 	trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
 	testX = testX.reshape((testX.shape[0], 28, 28, 1))
-# scale data to the range of [0, 1]
 trainX = trainX.astype("float32") / 255.0
 testX = testX.astype("float32") / 255.0
-# one-hot encode the training and testing labels
 trainY = np_utils.to_categorical(trainY, 10)
 testY = np_utils.to_categorical(testY, 10)
+
+
 # evaluate the model
 score = model.evaluate(testX, testY, verbose=0)
-labelNames = ["top", "trouser", "pullover", "dress", "coat",
-	"sandal", "shirt", "sneaker", "bag", "ankle boot"]
+
 
 print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
 preds = model.predict(testX)
+
+
+labelNames = ["top", "trouser", "pullover", "dress", "coat",
+	"sandal", "shirt", "sneaker", "bag", "ankle boot"]
 print(classification_report(testY.argmax(axis=1), preds.argmax(axis=1),
 	target_names=labelNames))
